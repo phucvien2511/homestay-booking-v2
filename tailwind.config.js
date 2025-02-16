282a0592083eeb6091ab26f979338307dc283ee3
Hien <timowerner11germany@gmail.com>
2025-02-16 17:19:40 +0700

    feat: add outline of landing page

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          start: '#3DBDB7',
+          end: '#6B42D8',
+        }
+      },
+    },
+  },
+  // ... rest of your config
+} 
